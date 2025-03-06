564b0e109aa41dfe88c50fca2c99e90fc9b099dc
KUSHALMANDALA07 <kushalmandala123@gmail.com>
2025-03-06 11:48:46 +0530

    funny

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
 
 app.get('/ping', (req, res) => {
     res.send('pong');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
