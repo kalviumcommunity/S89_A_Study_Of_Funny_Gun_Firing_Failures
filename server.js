856833d181b752e2c988a28e59cb314021378f61
KUSHALMANDALA07 <kushalmandala123@gmail.com>
2025-03-25 17:44:49 +0530

    done

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+
+app.get("/ping", (req, res) => {
+    try {
+        res.status(200).send({ msg: "pong" });
+    } catch (error) {
+        res.status(500).send({ msg: "Server error occurred", error: error.message });
+    }
+});
+
+
+app.listen(3000, async() => {
+    try{
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Server connected successfully")
+    }catch(error){
+        console.log("Error")
+    }
+    
+});
