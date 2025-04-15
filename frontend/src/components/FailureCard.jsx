e4b459c89b85ea280614d9f4cc9ed1e943e0a7da
KUSHALMANDALA07 <kushalmandala123@gmail.com>
2025-04-15 14:28:31 +0530

    Designing component

--- frontend/src/components/FailureCard.jsx
+++ frontend/src/components/FailureCard.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
-const FailureCard = ({ failure }) => {
+const FailuresCard = ({ gunFiring }) => {
   return (
-    <div className="border p-4 rounded shadow bg-white max-w-md my-4">
-      <h2 className="text-xl font-bold text-red-600 mb-1">🔫 {failure.title}</h2>
-      <p><strong>Location:</strong> {failure.location}</p>
-      <p><strong>Cause:</strong> {failure.cause}</p>
-      <p className="italic mt-2 text-gray-500">"{failure.comment}"</p>
+    <div>
+        <h1>Title: {gunFiring.title}</h1>
+        <p>Comment: {gunFiring.comment}</p>
     </div>
   );
 };
 
-export default FailureCard;
+export default FailuresCard;
